3a28103b55ae0b2b3f517f5628d4fed043d3a6e9
guitomesz <42675646+Guitomesz@users.noreply.github.com>
2023-11-25 14:02:35 -0300

    turning site more responsive

---
+++ src/javascript/responsive.js
@@ -0,0 +1,4 @@
+document.getElementById("showHeaderButton").addEventListener("click", function () {
+    document.querySelector("header").style.display = "flex";
+    document.getElementById("showHeaderButton").style.display = "none";
+});
