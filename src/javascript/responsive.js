424b053f9606820137981146a0e1e4a1a6b389f3
guitomesz <42675646+Guitomesz@users.noreply.github.com>
2023-11-25 14:31:54 -0300

    testing new functions

--- src/javascript/responsive.js
+++ src/javascript/responsive.js
@@ -1,4 +1,14 @@
-document.getElementById("showHeaderButton").addEventListener("click", function () {
-    document.querySelector("header").style.display = "flex";
-    document.getElementById("showHeaderButton").style.display = "none";
+document.addEventListener("DOMContentLoaded", function () {
+    // Adiciona um ouvinte de eventos para o botão
+    document.getElementById("toggleSidebarButtonSmartphone").addEventListener("click", function () {
+        // Obtém o elemento do cabeçalho
+        var header = document.querySelector("header");
+
+        // Alterna a visibilidade do cabeçalho
+        if (window.getComputedStyle(header).display === "none") {
+            header.style.display = "flex";
+        } else {
+            header.style.display = "none";
+        }
+    });
 });
